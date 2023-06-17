0b247736afb42735a5cf57c757517643842687cc
viettien1602 <viettien1602@gmail.com>
2023-06-17 19:43:59 +0700

    change firebase init

--- src/main/java/com/swd392/funfundbe/FunfundBeApplication.java
+++ src/main/java/com/swd392/funfundbe/FunfundBeApplication.java
@@ -15,6 +15,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -23,31 +25,57 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 @SpringBootApplication
-@SecurityScheme(name = "token_auth", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER, scheme = "bearer", bearerFormat = "JWT")
-@OpenAPIDefinition(info = @Info(title = "API Doc for Krowd Investment FunFund Web Service", description = "This is list of endpoints and documentations of REST API for FunFund Web Service", version = "1.0"), servers = {
+@SecurityScheme(
+		name = "token_auth",
+		type = SecuritySchemeType.HTTP,
+		in = SecuritySchemeIn.HEADER,
+		scheme = "bearer",
+		bearerFormat = "JWT"
+)
+@OpenAPIDefinition(
+	info = @Info(
+		title = "API Doc for Krowd Investment FunFund Web Service",
+		description = "This is list of endpoints and documentations of REST API for FunFund Web Service",
+		version = "1.0"
+	),
+	servers = {
 		@Server(url = "http://localhost:8080", description = "Local development server domain")
-}, security = {
-		@SecurityRequirement(name = "token_auth")
-}, tags = {
-		@Tag(name = "welcome", description = "REST API endpoint for welcome user")
-})
+	},
+	security = {
+			@SecurityRequirement(name = "token_auth")
+	},
+	tags = {
+			@Tag(name = "welcome", description = "REST API endpoint for welcome user")
+	}
+)
 @Slf4j
 public class FunfundBeApplication {
 
-	public static void main(String[] args) {
-		try {
-			FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
-			FirebaseOptions options = FirebaseOptions.builder()
-					.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-					.build();
-			FirebaseApp app = FirebaseApp.getApps().isEmpty()
-					? FirebaseApp.initializeApp(options)
-					: FirebaseApp.getInstance();
-			// FirebaseApp.initializeApp(options, "FunFundFirebaseApp");
-		} catch (IOException e) {
-			log.error(e.getMessage());
-			throw new RuntimeException(e);
+	@Primary
+	@Bean
+	public void initFirebase() throws IOException {
+		FirebaseOptions options = FirebaseOptions.builder()
+				.setCredentials(GoogleCredentials.fromStream(getClass().getClassLoader().getResourceAsStream("serviceAccountKey.json")))
+				.build();
+		if (FirebaseApp.getApps().isEmpty()) {
+			FirebaseApp.initializeApp(options);
 		}
+	}
+
+	public static void main(String[] args) {
+//		try {
+//			FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
+//			FirebaseOptions options = FirebaseOptions.builder()
+//					.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//					.build();
+//			FirebaseApp app = FirebaseApp.getApps().isEmpty()
+//					? FirebaseApp.initializeApp(options)
+//					: FirebaseApp.getInstance();
+//			// FirebaseApp.initializeApp(options, "FunFundFirebaseApp");
+//		} catch (IOException e) {
+//			log.error(e.getMessage());
+//			throw new RuntimeException(e);
+//		}
 		SpringApplication.run(FunfundBeApplication.class, args);
 	}
 
