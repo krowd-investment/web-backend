cbc1109e2f7614d4194a7de782f99c1f85f3a717
viettien1602 <viettien1602@gmail.com>
2023-07-19 21:44:42 +0700

    fix: return project with full fields

--- src/main/java/com/swd392/funfundbe/model/Response/ProjectResponse.java
+++ src/main/java/com/swd392/funfundbe/model/Response/ProjectResponse.java
@@ -1,5 +1,6 @@
 package com.swd392.funfundbe.model.Response;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 import lombok.AllArgsConstructor;
@@ -16,15 +17,24 @@ import lombok.Setter;
 public class ProjectResponse {
     private int projectId;
     private String projectName;
-    private String description;
-    private int areaId;
-    private String areaName;
-    private int fieldId;
-    private String fieldName;
     private String image;
-    private Date createAt;
+    private BigDecimal targetCapital;
+    private BigDecimal sharedRevenue;
+    private BigDecimal mutiplier;
+    private int duration;
+    private BigDecimal capital;
     private String brand;
     private Date startDate;
     private Date endDate;
     private String status;
+    private int numberOfStage;
+    private String projectDescription;
+    private int areaId;
+    private String areaName;
+    private int fieldId;
+    private String fieldName;
+    private BigDecimal investmentTargetCapital;
+    private BigDecimal paidAmount;
+    private BigDecimal remainingAmount;
+    private String businessLicense;
 }
